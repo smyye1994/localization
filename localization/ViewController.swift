4e0ba6e9c8130ba188991de04502d93fb8f75e15
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-09-02 13:36:10 +0300

    Initial Commit

---
+++ localization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  localization
+//
+//  Created by Sümeyye Kılıçoğlu on 2.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
