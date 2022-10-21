850d289515df9511e18cc2601a529583270616ed
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-10-21 16:16:15 +0300

    changing language

--- localization/ViewController.swift
+++ localization/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let myLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        
+       return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(myLabel)
+        myLabel.frame = view.bounds
     }
 
-
 }
-
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(
+            self,
+            tableName: "Locaizable",
+            bundle: .main,
+            value: self,
+            comment: self
+        )
+        
+    }
+}
